38b7d59196bcc8d142d0d2542f9496aef91b45f5
Renan Kosicki <renan@digi.ag>
2015-09-28 10:22:15 -0300

    Project created

---
+++ core-graphics/core-graphics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  core-graphics
+//
+//  Created by Renan Kosicki on 9/28/15.
+//  Copyright © 2015 Renan Kosicki | K-Mobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
